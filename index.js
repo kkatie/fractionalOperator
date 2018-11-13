82e6fc77a0ca7afeb3777b1b2f1c379a35e2fab0
Katie Khuu <kkhuu@carrentals.com>
2018-11-13 00:36:47 -0800

    Fix fraction initialization when parsing string statement

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import Fraction from 'Fraction';
 
+// parse operand string and return a Fraction object
+function convertOperandToFraction(operandString) {
+  // to-do: parse accordingly to get components
+  return new Fraction(wholeNumber, numerator, denominator);
+}
+
 // to-do: error-handling/check value of components
 export function fractionalOperator(stringStatement) {
-	var components = stringStatement.split(' ');
-	var operand1 = new Fraction(components[0]);
-	var operand2 = new Fraction(components[1]);
-	return Fraction.performOperation(operand1, operand2);
+  var components = stringStatement.split(' ');
+  var operand1 = convertOperandToFraction(components[0]);
+  var operand2 = convertOperandToFraction(components[1]);
+  var operator = components[2];
+  return Fraction.performOperation(operand1, operand2, operator);
 }
